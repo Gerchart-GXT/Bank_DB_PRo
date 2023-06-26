6289bd5b5a1bc5e62144f2d413f873f42ea591b2
Gerchart <gerchart.gxt@gmail.com>
2023-06-26 11:35:57 +0800

    finish

--- Demos/calculator/src/jsx/Redux/action.jsx
+++
@@ -1,8 +0,0 @@
-const ACTIONS = {
-    ADD_DIGIT: "add-digit",
-    DELETE_DIGIT: "delete-digit",
-    CLEAR: "clear",
-    EVALUATE: "evaluate"
-};
-
-export default ACTIONS;
--- Demos/calculator/src/jsx/Redux/store.jsx
+++
@@ -1,9 +0,0 @@
-import ACTIONS from "./action";
-import reducer from "./reducer";
-import { configureStore } from '@reduxjs/toolkit';
-
-const store = configureStore({
-    reducer
-});
-
-export default store;
